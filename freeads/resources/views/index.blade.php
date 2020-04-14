d0914eaf41cbfba349de87c0b7980f13b4c5e315
unknown <edouardnoel.tardif@gmail.com>
2020-04-14 18:34:56 +0200

    Auth + Emailverif

---
+++ freeads/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    L'INDEX
+    <a href="/login">login</a>
+    <a href="/register">REGISTER</a>
+
+</body>
+</html>
--- freeads/routes/web.php
+++ freeads/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','IndexController@showindex');
+
+// Route::get('/',function(){ return view('welcome');});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
